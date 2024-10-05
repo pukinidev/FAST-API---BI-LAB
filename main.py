4a3a80a3c40e77d7aaf62645c6df64be4b5c70a7
pukinidev <v.caro@uniandes.edu.co>
2024-10-05 01:58:32 -0500

    fixes!

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from fastapi import FastAPI
 from DataModel import DataModel
 from joblib import load, dump
 from Preprocessing import Preprocessing
+from typing import List
 
 app = FastAPI()
 
 @app.post("/predict")
-def make_predictions(dataModel: DataModel):
-    data = dataModel.model_dump()
-    df = pd.DataFrame([data], columns=data.keys(), index=[0])
+def make_predictions(data_models: List[DataModel]):
+    data_list = [data_model.model_dump() for data_model in data_models]
+    df = pd.DataFrame(data_list)
     cleaner = Preprocessing()
     data_cleaned = cleaner.fit_transform(df)
     x = data_cleaned.drop("Duracion_Estancia_Min", axis=1)
     model = load("assets/modelo.joblib")
     result = model.predict(x)
-    return result.tolist()
+    result_list = result.tolist()
+    return result_list
